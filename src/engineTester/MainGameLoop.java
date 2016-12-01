80f461a8eb70189556914713b3a29cb1a432d3a3
rsampdev <rsampdev@gmail.com>
2016-12-01 17:59:51 -0500

    Vertex, Fragment and Static shaders setup

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -6,6 +6,7 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
 
 public class MainGameLoop {
 
@@ -14,6 +15,7 @@ public class MainGameLoop {
 
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
+		StaticShader shader = new StaticShader();
 
 		float[] vertices = { // vertices
 				-0.5f, 0.5f, 0, // V0
@@ -30,14 +32,16 @@ public class MainGameLoop {
 		RawModel model = loader.loadToVAO(vertices, indices);
 
 		while (!Display.isCloseRequested()) {
-			renderer.prepare();
-
 			// game logic
-
+			
+			renderer.prepare();
+			shader.start();
 			renderer.render(model);
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}

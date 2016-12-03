0caafb4ecaeb21e605a8cbce3e0a62139f9628f0
rsampdev <rsampdev@gmail.com>
2016-12-02 22:48:01 -0500

    Added ambient lighting and MasterRenderer class for optimized rendering

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,57 +1,59 @@
 package engineTester;
- 
-import models.RawModel;
-import models.TexturedModel;
- 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
- 
-import renderEngine.DisplayManager;
-import renderEngine.Loader;
-import renderEngine.OBJLoader;
-import renderEngine.Renderer;
-import shaders.StaticShader;
-import textures.ModelTexture;
+
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
- 
+import models.RawModel;
+import models.TexturedModel;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.MasterRenderer;
+import renderEngine.OBJLoader;
+import textures.ModelTexture;
+
 public class MainGameLoop {
- 
-    public static void main(String[] args) {
- 
-        DisplayManager.createDisplay();
-        Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
-        
+
+	public static void main(String[] args) {
+
+		DisplayManager.createDisplay();
+		Loader loader = new Loader();
+		List<Entity> entities = new ArrayList<Entity>();
+
 		RawModel model = OBJLoader.loadObjModel("models/dragon/model", loader);
 		ModelTexture texture = new ModelTexture(loader.loadTexture("models/dragon/texture"));
 		texture.setShineDamper(10);
 		texture.setReflectivity(1);
-		
+
 		TexturedModel texturedModel = new TexturedModel(model, texture);
 		Entity entity = new Entity(texturedModel, new Vector3f(0, 0, -25), 0, 0, 0, 1);
-		
+		entities.add(entity);
+
 		Light light = new Light(new Vector3f(0, 0, -20), new Vector3f(1, 1, 1));
-        Camera camera = new Camera();
-        
-        while(!Display.isCloseRequested()){
-            entity.increaseRotation(0, 0, 0);
-            camera.move();
-            renderer.prepare();
-            shader.start();
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity,shader);
-            shader.stop();
-            DisplayManager.updateDisplay();
-        }
- 
-        shader.cleanUp();
-        loader.cleanUp();
-        DisplayManager.closeDisplay();
- 
-    }
- 
+		Camera camera = new Camera();
+		MasterRenderer renderer = new MasterRenderer();
+
+		while (!Display.isCloseRequested()) {
+			camera.move();
+
+			for (Entity e : entities) {
+				renderer.processEntity(e);
+			}
+
+			renderer.render(light, camera);
+
+			DisplayManager.updateDisplay();
+		}
+
+		renderer.cleanUp();
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+
+	}
+
 }
